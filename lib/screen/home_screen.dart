12dbe9fce82f18508b511e6c7dd168c21dd67cfa
hafid <hafid.siraj@vadhana.co.id>
2024-02-18 14:53:35 +0700

    [FEATURE] add how audio player work

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,9 @@
+import 'package:audioplayer_project/provider/audio_notifier.dart';
+import 'package:audioplayer_project/utils/utils.dart';
 import 'package:audioplayer_project/widget/audio_controller_widget.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widget/buffer_slider_controller_widget.dart';
 
@@ -12,6 +15,47 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late final AudioPlayer audioPlayer;
+  late final Source audioSource;
+
+  @override
+  void initState() {
+    final provider = context.read<AudioNotifier>();
+    audioPlayer = AudioPlayer();
+    // audioSource = AssetSource("cricket.wav");
+    audioSource = UrlSource(
+        "https://github.com/dicodingacademy/assets/raw/main/flutter_intermediate_academy/bensound_ukulele.mp3");
+    audioPlayer.setSource(audioSource);
+
+    audioPlayer.onPlayerStateChanged.listen((state) {
+      provider.isPlay = state == PlayerState.playing;
+      if (state == PlayerState.stopped) {
+        provider.position = Duration.zero;
+      }
+    });
+
+    audioPlayer.onDurationChanged.listen((duration) {
+      provider.duration = duration;
+    });
+
+    audioPlayer.onPositionChanged.listen((position) {
+      provider.position = position;
+    });
+
+    audioPlayer.onPlayerComplete.listen((_) {
+      provider.position = Duration.zero;
+      provider.isPlay = false;
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    audioPlayer.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,19 +65,38 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          BufferSliderControllerWidget(
-            maxValue: 5,
-            currentValue: 1,
-            minText: "Current Duration",
-            maxText: "Maximum Duration",
-            onChanged: (value) async {},
-          ),
-          AudioControllerWidget(
-            onPlayTapped: () {},
-            onPauseTapped: () {},
-            onStopTapped: () {},
-            isPlay: false,
-          ),
+          Consumer<AudioNotifier>(builder: (context, provider, child) {
+            final duration = provider.duration;
+            final position = provider.position;
+
+            return BufferSliderControllerWidget(
+              maxValue: duration.inSeconds.toDouble(),
+              currentValue: position.inSeconds.toDouble(),
+              minText: durationToTimeString(position),
+              maxText: durationToTimeString(duration),
+              onChanged: (value) async {
+                final newPosition = Duration(seconds: value.toInt());
+                await audioPlayer.seek(newPosition);
+                await audioPlayer.resume();
+              },
+            );
+          }),
+          Consumer<AudioNotifier>(builder: (context, provider, child) {
+            final bool isPlay = provider.isPlay;
+
+            return AudioControllerWidget(
+              onPlayTapped: () {
+                audioPlayer.play(audioSource);
+              },
+              onPauseTapped: () {
+                audioPlayer.pause();
+              },
+              onStopTapped: () {
+                audioPlayer.stop();
+              },
+              isPlay: isPlay,
+            );
+          }),
         ],
       ),
     );
